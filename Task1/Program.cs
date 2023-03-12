01575c86d69fa568d5b1bcf53c336fc36eebc086
Dmitry Vasiliuk <usy222@mail.ru>
2023-03-12 19:48:18 +0300

    В двумерном массиве сортируем элем строк по убыванию

---
+++ Task1/Program.cs
@@ -0,0 +1,107 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Task1();
+
+void Task1()
+{
+  Console.WriteLine();
+  int firstLength = ReadInt("First Length");
+  int secondLength = ReadInt("Second Length");
+
+  int[,] array = CreatTwoDimensionArray(firstLength, secondLength); 
+  Console.WriteLine(PrintTwoDimensionArray(array));
+
+  CreatTwoDimensionOrderedArray(array);
+  PrintTwoDimensionOrderArray(array);
+}
+
+int ReadInt(string argument) 
+{
+  Console.Write($"Input {argument}: ");
+  int result = 0;
+
+  while (!int.TryParse(Console.ReadLine(), out result))
+  {
+    Console.WriteLine("Try again");
+  }
+
+  return result;
+}
+
+int[,] CreatTwoDimensionArray(int firstLength, int secondLength ) 
+{
+  Console.WriteLine();
+
+  int[,] result = new int[firstLength, secondLength]; 
+  Random rnd = new Random(); 
+
+  for (int i = 0; i < result.GetLength(0); i++) 
+  {
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+      result[i, j] = rnd.Next(1, 10);
+    }
+  }
+
+  return result;
+}
+
+int[,] CreatTwoDimensionOrderedArray(int[,] array) 
+{
+  int[,] result = new int[array.GetLength(0), array.GetLength(1)]; 
+  
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+string PrintTwoDimensionArray(int[,] array) 
+{
+  string result = string.Empty; 
+
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      result += $"{array[i,j]} "; 
+    }
+    
+    result += Environment.NewLine; 
+  }
+
+  return result;
+}
+
+void PrintTwoDimensionOrderArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
